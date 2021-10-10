877e27a21a703b8e16925e849e8c95fe7854dded
Wesley Endliche <wesleyhoo@hotmail.com>
2021-10-10 09:16:19 -0300

    created exercises 8 to 11

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`Your car speed is ${vel}km/h.`)
+if (vel > 60){
+    console.log(`You are speeding. HERE'S A TRAFFIC TICKET!`)
+}
+console.log(`Don't forget to wear your seatbelt!`)
---
+++ ex009.js
@@ -0,0 +1,7 @@
+var country = 'USA'
+console.log(`Living in ${country}.`)
+if (country == 'Canada') {
+    console.log('You are Canadian!')
+} else {
+    console.log('You are a foreigner.')
+}
