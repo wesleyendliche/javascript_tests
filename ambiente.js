2e2f13dd288576f1631e2bec0f26b7d871916217
Wesley Endliche <wesleyhoo@hotmail.com>
2021-10-12 16:23:27 -0300

    structures_for_do_while

---
+++ ambiente.js
@@ -0,0 +1,18 @@
+console.log('Vai começar...')
+for (var c = 1; c <= 10; c++) {
+    console.log(c)
+}
+console.log('FIM!')
+
+/*var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++  
+} while (c <= 6)
+
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ 
+}
+
+console.log('Tudo bem?')*/
