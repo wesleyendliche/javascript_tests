e3f41c81239fc5e4671ff5ef84b5c33ff2a0c3ec
Wesley Endliche <wesleyhoo@hotmail.com>
2021-10-17 08:28:32 -0300

    update tabuada

--- exercises_video_4/script_2.js
+++ exercises_video_4/script_2.js
@@ -1,42 +1,18 @@
-function verificar() {
-    var data = new Date()
-    var ano =data.getFullYear()
-    var fAno = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
-    if (fAno.value.length == 0 || fAno.value > ano) {
-        window.alert('Verifique os dados e tente novamente')
+function multiplicar() {
+    let num = document.getElementById('num')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número')
     } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fAno.value)
-        var genero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto') //similar to typing id in HTML
-        if (fsex[0].checked) {
-            genero = 'Homem'
-            if (idade >= 0 && idade < 10) {
-                img.setAttribute('src', 'baby-mf.png')
-            } else if (idade < 21) {
-                img.setAttribute('src', 'young-m.png')
-            } else if (idade < 50) {
-                img.setAttribute('src', 'adult-m.png')
-            } else {
-                img.setAttribute('src', 'elderly-m.png')
-            }
-        } else if (fsex[1].checked) {
-            genero = 'Mulher'
-            if (idade >= 0 && idade < 10) {
-                img.setAttribute('src', 'baby-mf.png')
-            } else if (idade < 21) {
-                img.setAttribute('src', 'young-f.png')
-            } else if (idade < 50) {
-                img.setAttribute('src', 'adult-f.png')
-            } else {
-                img.setAttribute('src', 'elderly-f.png')
-            }
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
         }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
-        res.appendChild(img)
     }
-
 }
