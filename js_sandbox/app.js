449eaf4809e2402d08ed008e5b81bd4a0ac62bcf
Wesley Endliche <wesleyhoo@hotmail.com>
2021-11-03 13:44:27 -0300

    console.... tests

---
+++ js_sandbox/app.js
@@ -0,0 +1,20 @@
+console.log('Hello World!');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('This is an error');
+console.clear();
+console.warn('this is a warn')
+console.time('Hello');
+console.log('Hello World!');
+console.log('Hello World!');
+console.log('Hello World!');
+console.log('Hello World!');
+console.log('Hello World!');
+console.log('Hello World!');
+console.timeEnd('Hello');
