a85723954bde834c33b85b706bb2977bb02c235f
Wesley Endliche <wesleyhoo@hotmail.com>
2021-11-03 14:28:25 -0300

    some more tests

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,20 +1,41 @@
-console.log('Hello World!');
-console.log(123);
-console.log(true);
-var greeting = 'Hello';
-console.log(greeting);
-console.log([1,2,3,4]);
-console.log({a:1, b:2});
-console.table({a:1, b:2});
+let val;
 
-console.error('This is an error');
-console.clear();
-console.warn('this is a warn')
-console.time('Hello');
-console.log('Hello World!');
-console.log('Hello World!');
-console.log('Hello World!');
-console.log('Hello World!');
-console.log('Hello World!');
-console.log('Hello World!');
-console.timeEnd('Hello');
+// Number to string
+val = String(555);
+val = String(4+4);
+// Boolean to string
+val  = String(true);
+//Date to string
+val = String(new Date());
+// Array to string
+val = String([1,2,3,4]);
+
+// toString()
+val = (5).toString();
+val = (true).toString();
+
+// To number
+val = Number('5');
+val = Number(true); //1
+val = Number(false); //0
+val = Number(null); //0
+val = Number('Hello'); //NaN
+val = Number([1,2,3]); //NaN
+
+// Another method
+val = parseInt('100.30'); //100
+val = parseFloat('100.30'); //100.3
+
+
+// Output
+//console.log(val);
+//console.log(typeof val);
+//console.log(val.length); //only works on strings
+//console.log(val.toFixed(2)); //only works on numbers
+
+const val1 = String(5);
+const val2 = 6;
+const sum = Number(val1 + val2); //56, number
+
+console.log(sum);
+console.log(typeof sum);
